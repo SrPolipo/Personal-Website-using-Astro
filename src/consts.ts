ab30b5a3bd04ffd654587dd381cbdb19922cfb1e
SrPolipo <94134986+SrPolipo@users.noreply.github.com>
2026-01-26 21:27:45 +0000

    Added the planets

--- src/consts.ts
+++ src/consts.ts
@@ -4,8 +4,8 @@ export const SITE: Site = {
   TITLE: "Rafael Hipolito",
   DESCRIPTION: "Personal website of Rafael Hipolito",
   EMAIL: "contact@rafaelhipolito.xyz",
-  NUM_POSTS_ON_HOMEPAGE: 5,
-  NUM_PROJECTS_ON_HOMEPAGE: 0,
+  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
